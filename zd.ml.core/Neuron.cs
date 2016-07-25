4e094e92ce1037271f1b5e8baf3e36b8d0834a05
zeng-dong <zdongdong@yahoo.com>
2016-07-25 10:43:35 -0500

    initial solution and project

---
+++ zd.ml.core/Neuron.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd.ml.core
+{
+    public class Neuron
+    {
+        public double[] Weights { get; set; }
+        public double[] PrevWeights { get; set; }
+    }
+}
